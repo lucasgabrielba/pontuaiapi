66e4c31604afa43c48902af61cd67e130a762c67
lucas gabriel <lucasgabrielba@gmail.com>
2025-05-14 00:54:30 -0300

    feat: Register AIServiceProvider in the application service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
+    App\Providers\AIServiceProvider::class,
     Laravel\Telescope\TelescopeServiceProvider::class,
 ];
